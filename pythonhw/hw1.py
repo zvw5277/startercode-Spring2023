804ae05540606d9ac3af20e84d21e3aaf579144d
Daniel Kifer <dkifer@cse.psu.edu>
2023-01-10 20:41:40 -0500

    python hw

---
+++ pythonhw/hw1.py
@@ -0,0 +1,26 @@
+# use spaces. Do NOT use tabs.
+
+def q1(mystring):
+    pass
+
+
+def q2(mystring):
+    pass
+
+
+
+def q3(myarray):
+    pass
+
+
+def q4(myarray):
+    """ 
+    you cannot change how the array is iterated 
+    and you cannot use any list operations on myarray """
+
+    for mystring in mystringarray:
+        pass
+
+
+def q5(myarray):
+    pass
