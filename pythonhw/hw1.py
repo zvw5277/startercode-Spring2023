0d89a2bbee607699856f7dc3036c601fd4544c10
zvw5277 <zvw5277@psu.edu>
2023-01-18 18:32:33 -0500

    HW1 Completed

--- pythonhw/hw1.py
+++ pythonhw/hw1.py
@@ -1,6 +1,7 @@
 # use spaces. Do NOT use tabs.
 
 def q1(mystring):
+   #mystring = 'Spring\tSemester\t2023 began this\tweek'
     parts = mystring.split('\t')
     tuple1 = parts[1]
     tuple2 = parts[2]
@@ -9,22 +10,40 @@ def q1(mystring):
 
 
 def q2(mystring):
-    pass
-
+   #mystring = "a\tthe\t the \tthe\tthen"
+    string_parts = mystring.split("\t")
+    return (string_parts.count("the"))
 
 
 def q3(myarray):
-    pass
+   #myarray = [("A", 2), ("B", 5), ("A", 2), ("A", 10)]
+    summation = 0
+    for part in myarray:
+        if part[0] == "A":
+            summation += part[1]
+    return (summation)
 
 
 def q4(myarray):
     """ 
     you cannot change how the array is iterated 
     and you cannot use any list operations on myarray """
-
-    for mynum in mystringarray:
-        pass
+    #myarray = [1, 1.5, 3, 7, 9, 10, 1]
+    counter = 0
+    for mynum in myarray:
+        if mynum % 2 != 0:
+            counter += 1
+        elif mynum % 2 == 0:
+            return (counter)
+    return (-1)
 
 
 def q5(myarray):
-    pass
+    #myarray = [('c', 2), ('a', 3), ('d', 3), ('a', 2), ('e', 1), ('c', 1)]
+    dictcount = {}
+    for i in myarray:
+        if i[0] in dictcount:
+            dictcount[i[0]] = dictcount.get(i[0]) + i[1]
+        else:
+            dictcount[i[0]] = i[1]
+    return (dictcount)
